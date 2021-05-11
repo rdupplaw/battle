479b20ce3e4462f768d6421adb461c18c2df6325
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-11 15:25:07 +0100

    initial commit

---
+++ app.rb
@@ -0,0 +1,14 @@
+require 'rack'
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Battle < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    'test'
+  end
+end
+
