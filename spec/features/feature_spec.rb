a862e64512b665fe6fa4f0db883d00e056c124c3
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-11 16:40:45 +0100

    show player 2 hit points

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -7,3 +7,13 @@ feature 'Two players'  do
     expect(page).to have_content('Rob vs James')
   end
 end
+
+feature 'Hit points' do
+  scenario 'Display player 2 hit points' do
+    visit('/')
+    fill_in('Player 1 Name', with: 'Rob')
+    fill_in('Player 2 Name', with: 'James')
+    click_button('Submit')
+    expect(page).to have_content('Player 2 Hit Points: 100')
+  end
+end
