cd50713e3ea70da0a580d1925593780a4ba42741
James McMullan <73468134+JamesMcMull@users.noreply.github.com>
2021-05-11 16:31:45 +0100

    refactor post into get with session

--- app.rb
+++ app.rb
@@ -6,15 +6,21 @@ class Battle < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player1 = params[:'Player 1 Name']
-    @player2 = params[:'Player 2 Name']
-    erb :play
+    session[:player1] = params[:'Player 1 Name']
+    session[:player2] = params[:'Player 2 Name']
+    redirect '/play'
+  end
+  get '/play' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
+  erb :play
   end
 end
 
